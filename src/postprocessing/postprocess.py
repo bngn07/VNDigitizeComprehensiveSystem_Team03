6f6f30485294a745c6a37b073fe646413fc37c07
kunnic <official.nguyenduchuy@gmail.com>
2026-05-11 15:58:16 +0700

    feat: update concat func to cat words on a row in a Postprocess() object

--- src/postprocessing/postprocess.py
+++ src/postprocessing/postprocess.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Union
 
-from ...ocr import OCRResult, TextBlock
+from ocr.ocr import OCRResult, TextBlock
 
 # @dataclass
 # class PostprocessResult:
